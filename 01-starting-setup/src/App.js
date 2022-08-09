22b3c90b82bce9af827b90470f4b8f65bb944a82
roshanwaa <kumroshan120@gmail.com>
2022-08-09 14:25:24 +0530

    Start React app

---
+++ 01-starting-setup/src/App.js
@@ -0,0 +1,11 @@
+import ExpensesItem from './Components/ExpensesItem.js';
+function App() {
+  return (
+    <div>
+      {/* <h2>Let's get started!</h2> */}
+      <ExpensesItem />
+    </div>
+  );
+}
+
+export default App;
