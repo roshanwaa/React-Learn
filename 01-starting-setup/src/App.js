06f36156e13ab989306f7526a001740c578b6965
Mr.Smoke <kumroshan120@gmail.com>
2022-08-10 20:50:25 +0530

    add Componates

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,10 +1,36 @@
 import Expenses from './Components/Expenses.js';
 
 function App() {
-  // console.log(myExpenses[0].title);
+  const myExpenses = [
+    {
+      id: 'e1',
+      title: 'Chai',
+      amt: 500,
+      date: new Date(2022, 6, 15),
+    },
+    {
+      id: 'e2',
+      title: 'Travel',
+      amt: 300,
+      date: new Date(2022, 5, 25),
+    },
+    {
+      id: 'e3',
+      title: 'Petrol',
+      amt: 1000,
+      date: new Date(2022, 5, 30),
+    },
+    {
+      id: 'e4',
+      title: 'Food',
+      amt: 2000,
+      date: new Date(2022, 6, 2),
+    },
+  ];
+  console.log(myExpenses[0]);
   return (
     <div>
-      <Expenses></Expenses>
+      <Expenses items={myExpenses}></Expenses>
     </div>
   );
 }
--- 01-starting-setup/src/Components/Expenses.js
+++ 01-starting-setup/src/Components/Expenses.js
@@ -2,54 +2,28 @@ import React from 'react';
 import ExpensesItem from './ExpensesItem.js';
 import './Expenses.css';
 
-export default function Expenses() {
-  const myExpenses = [
-    {
-      id: 'e1',
-      title: 'Chai',
-      amt: 500,
-      date: new Date(2022, 6, 15),
-    },
-    {
-      id: 'e2',
-      title: 'Travel',
-      amt: 300,
-      date: new Date(2022, 5, 25),
-    },
-    {
-      id: 'e3',
-      title: 'Petrol',
-      amt: 1000,
-      date: new Date(2022, 5, 30),
-    },
-    {
-      id: 'e4',
-      title: 'Food',
-      amt: 2000,
-      date: new Date(2022, 6, 2),
-    },
-  ];
+export default function Expenses(props) {
   return (
     <div className="expenses">
       <ExpensesItem
-        title={myExpenses[0].title}
-        amount={myExpenses[0].amt}
-        date={myExpenses[0].date}
+        title={props.items[0].title}
+        amount={props.items[0].amt}
+        date={props.items[0].date}
       />
       <ExpensesItem
-        title={myExpenses[1].title}
-        amount={myExpenses[1].amt}
-        date={myExpenses[1].date}
+        title={props.items[1].title}
+        amount={props.items[1].amt}
+        date={props.items[1].date}
       />
       <ExpensesItem
-        title={myExpenses[2].title}
-        amount={myExpenses[2].amt}
-        date={myExpenses[2].date}
+        title={props.items[2].title}
+        amount={props.items[2].amt}
+        date={props.items[2].date}
       />
       <ExpensesItem
-        title={myExpenses[3].title}
-        amount={myExpenses[3].amt}
-        date={myExpenses[3].date}
+        title={props.items[3].title}
+        amount={props.items[3].amt}
+        date={props.items[3].date}
       />
     </div>
   );
