701108249e75037aea63146a89298d9f52777e6b
Mr.Smoke <kumroshan120@gmail.com>
2022-09-08 22:48:04 +0530

    Adding a "User" Component

--- React project/src/App.js
+++ React project/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { AddUsers } from './Components/Users/AddUsers';
 
 function App() {
   return (
     <div>
-
+      <h1>Hello Roshan</h1>
+      <AddUsers />
     </div>
   );
 }
