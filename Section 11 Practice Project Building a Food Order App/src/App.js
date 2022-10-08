d06df3e4ba4c9ab9594ae876054a0835ae09365a
Mr.Smoke <kumroshan120@gmail.com>
2022-10-08 09:38:14 +0530

    Header Done

--- Section 11 Practice Project Building a Food Order App/src/App.js
+++ Section 11 Practice Project Building a Food Order App/src/App.js
@@ -1,9 +1,9 @@
-function App() {
+import React from 'react';
+import { Header } from './Components/Layout/Header';
+export const App = () => {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <Header />
+    </>
   );
-}
-
-export default App;
+};
---
+++ Section 11 Practice Project Building a Food Order App/src/Components/Layout/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './Header.module.css';
+// import mealsImg from '../../Assets/meals.jpg';
+export const Header = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <img
+          src="https://images.yourstory.com/cs/images/companies/ChaiSutta-1637585790850.jpg?fm=auto&ar=1:1&mode=fill&fill=solid&fill-color=fff"
+          alt=""
+        />
+        <h1>Chai Sutta Bar</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}>
+        {/* <img className={classes['main-image']} src={mealsImg} alt="chai" /> */}
+        <img
+          src="https://images.unsplash.com/photo-1638204380661-db83014e4a7b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+          alt="chai"
+        />
+      </div>
+    </>
+  );
+};
