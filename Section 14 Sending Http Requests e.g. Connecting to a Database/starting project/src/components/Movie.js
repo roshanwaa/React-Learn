63e9c2f2190f722bad4a2f0c8793b536d20de466
Mr.Smoke <kumroshan120@gmail.com>
2022-11-15 23:19:13 +0530

    Sending a GET Request

--- Section 14 Sending Http Requests e.g. Connecting to a Database/starting project/src/components/Movie.js
+++ Section 14 Sending Http Requests e.g. Connecting to a Database/starting project/src/components/Movie.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
 
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+  const unique_id = uuid();
   return (
-    <li className={classes.movie}>
+    <li className={classes.movie} key={unique_id}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <p>{props.characters}</p>
     </li>
   );
 };
