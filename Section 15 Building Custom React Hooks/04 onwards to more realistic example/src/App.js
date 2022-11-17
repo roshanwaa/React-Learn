812403d6a120d5d85edf0485b24423f5f8e4d47c
Mr.Smoke <kumroshan120@gmail.com>
2022-11-18 00:20:34 +0530

    192. Using the Custom Http Hook

--- Section 15 Building Custom React Hooks/04 onwards to more realistic example/src/App.js
+++ Section 15 Building Custom React Hooks/04 onwards to more realistic example/src/App.js
@@ -2,8 +2,61 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttps from './hooks/use-https';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const transformTask = (tasksObj) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  };
+
+  // const { isLoading, error, sendRequest } = httpData;
+
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       'https://react-http-6b4a6.firebaseio.com/tasks.json'
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error('Request failed!');
+  //     }
+
+  //     const data = await response.json();
+  //   } catch (err) {
+  //     setError(err.message || 'Something went wrong!');
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttps(
+    {
+      url: 'https://react-movie-app-7b37a-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',
+    },
+    transformTask
+  );
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
   return (
     <React.Fragment>
       <NewTask onAddTask={taskAddHandler} />
--- Section 15 Building Custom React Hooks/04 onwards to more realistic example/src/hooks/use-https.jsx
+++ Section 15 Building Custom React Hooks/04 onwards to more realistic example/src/hooks/use-https.jsx
@@ -9,9 +9,9 @@ const useHttps = (requestConfig, applyData) => {
     setError(null);
     try {
       const response = await fetch(requestConfig.url, {
-        method: requestConfig.method,
-        headers: requestConfig.headers,
-        body: JSON.stringify(requestConfig.body),
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        header: requestConfig.header ? requestConfig.header : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
 
       if (!response.ok) {
