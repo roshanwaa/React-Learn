9839857efd98221219778c384ab74d5b36f0a1e8
Mr.Smoke <kumroshan120@gmail.com>
2022-11-25 09:35:29 +0530

    200. Adding Basic Validation

--- Section 16 Working with Forms and User Input/01-starting-project/src/components/SimpleInput.js
+++ Section 16 Working with Forms and User Input/01-starting-project/src/components/SimpleInput.js
@@ -6,16 +6,23 @@ const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
 
   const nameInputChangeHandler = (e) => {
-    // its took the current value of the input filed by using useState
+    // * its took the current value of the input filed by using useState
     setEnteredName(e.target.value);
     console.log(e.target.value);
   };
 
   const formSubmitHandler = (e) => {
-    // set a default event or page. the page is not reloaded anymore
+    // * set a default event or page. the page is not reloaded anymore
     e.preventDefault();
 
-    // its also took the current value of the input filed by using ref
+    // * check the input is empty and then exit the submit handler
+    if (enteredName.trim() === '') {
+      // if the input is empty then exit the submit handler and alert a message
+      alert('Please enter a valid name');
+      return;
+    }
+
+    //* its also took the current value of the input filed by using ref
     const enteredValue2 = nameInputRef.current.value;
 
     console.log(enteredName);
@@ -23,7 +30,7 @@ const SimpleInput = (props) => {
 
     // * It will clear the value of the input field by using ref and its works but its not a ideal solution because we are directly manipulating the dom here,by using vanilla js to directly reach out to the dom and change something there.
 
-    // nameInputRef.current.value = ''; // * ==> Not ideal, don't manipulate the DOM
+    // nameInputRef.current.value = ''; // ! ==> Not ideal, don't manipulate the DOM
 
     // * It will clear the value of the input field
     setEnteredName('');
