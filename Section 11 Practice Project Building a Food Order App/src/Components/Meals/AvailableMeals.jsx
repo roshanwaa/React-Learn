3c61e96faf488b80b4328f62383c05ea077e752f
Mr.Smoke <kumroshan120@gmail.com>
2022-12-11 14:41:32 +0530

    214. Moving Meals Data To The Backend

--- Section 11 Practice Project Building a Food Order App/src/Components/Meals/AvailableMeals.jsx
+++ Section 11 Practice Project Building a Food Order App/src/Components/Meals/AvailableMeals.jsx
@@ -3,32 +3,32 @@ import classes from './AvailableMeals.module.css';
 import { Card } from '../UI/Card';
 import { MealsItem } from './MealsItems/MealsItem';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Chai',
-    description: 'Finest fish and veggies',
-    price: 22,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: 'm1',
+//     name: 'Chai',
+//     description: 'Finest fish and veggies',
+//     price: 22,
+//   },
+//   {
+//     id: 'm2',
+//     name: 'Schnitzel',
+//     description: 'A german specialty!',
+//     price: 16.5,
+//   },
+//   {
+//     id: 'm3',
+//     name: 'Barbecue Burger',
+//     description: 'American, raw, meaty',
+//     price: 12.99,
+//   },
+//   {
+//     id: 'm4',
+//     name: 'Green Bowl',
+//     description: 'Healthy...and green...',
+//     price: 18.99,
+//   },
+// ];
 
 export const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => {
