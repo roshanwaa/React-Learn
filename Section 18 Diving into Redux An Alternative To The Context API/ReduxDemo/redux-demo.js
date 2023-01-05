950fd3bff3dcd079dc254bfc5f6d62306ef8429e
Mr.Smoke <kumroshan120@gmail.com>
2023-01-05 22:04:21 +0530

    230. Exploring The Core Redux Concepts

--- Section 18 Diving into Redux An Alternative To The Context API/ReduxDemo/redux-demo.js
+++ Section 18 Diving into Redux An Alternative To The Context API/ReduxDemo/redux-demo.js
@@ -1,4 +1,27 @@
 const redux = require('redux');
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
-const store = createStore();
+const counterReducer = (state = { counter: 0 }, action) => {
+  console.log('counterReducer Call');
+
+  //   console.log(state);
+  return {
+    counter: state.counter + 1,
+  };
+};
+
+// console.log(counterReducer({ counter: 10 }));
+
+const store = redux.createStore(counterReducer);
+
+// console.log(store.getState());
+
+const counterSubscriber = () => {
+  //   const latestState = store.getState(state.counter).subscribe;
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'counter' });
