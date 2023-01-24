70a03d71fa11bfe18e03481eb09bbc33e49f1037
Mr.Smoke <kumroshan120@gmail.com>
2023-01-24 16:10:38 +0530

    235. Using Redux Data in React Components

---
+++ Section 18 Diving into Redux An Alternative To The Context API/Starting Up/redux-demo.js
@@ -0,0 +1,28 @@
+const redux = require('redux');
+// import { createStore } from 'redux';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'INCREMENT') {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === 'DECREMENT') {
+    return { counter: state.counter - 5 };
+  }
+  // console.log('counterReducer Call');
+};
+
+// console.log(counterReducer({ counter: 10 }));
+
+// Central part of redux
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'INCREMENT' });
+
+store.dispatch({ type: 'DECREMENT' });
--- Section 18 Diving into Redux An Alternative To The Context API/Starting Up/src/components/Counter.js
+++ Section 18 Diving into Redux An Alternative To The Context API/Starting Up/src/components/Counter.js
@@ -1,14 +1,17 @@
 import classes from './Counter.module.css';
-
+import { useSelector } from 'react-redux';
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {
-    
+    console.log('Button Click');
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>COUNTER VALUE</div>
+      <div className={classes.value}>COUNTER</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
