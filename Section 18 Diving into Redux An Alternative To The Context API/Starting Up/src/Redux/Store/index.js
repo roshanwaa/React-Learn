81b7f4d4011ddb7fb7a55b0aa091c3072b357f5b
Mr.Smoke <kumroshan120@gmail.com>
2023-01-24 16:20:28 +0530

    236. Dispatching Actions From Inside Components

--- Section 18 Diving into Redux An Alternative To The Context API/Starting Up/src/Redux/Store/index.js
+++ Section 18 Diving into Redux An Alternative To The Context API/Starting Up/src/Redux/Store/index.js
@@ -2,7 +2,7 @@ import { createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === 'INCREMENT') {
-    return { counter: state.counter + 1 };
+    return { counter: state.counter + 1,  };
   }
   if (action.type === 'DECREMENT') {
     return { counter: state.counter - 1 };
--- Section 18 Diving into Redux An Alternative To The Context API/Starting Up/src/components/Counter.js
+++ Section 18 Diving into Redux An Alternative To The Context API/Starting Up/src/components/Counter.js
@@ -1,7 +1,19 @@
 import classes from './Counter.module.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+
+  const dispatch = useDispatch();
+
+  const incrementCounterHandler = () => {
+    dispatch({ type: 'INCREMENT' });
+    console.log('Increment Click');
+  };
+  const decrementCounterHandler = () => {
+    dispatch({ type: 'DECREMENT' });
+
+    console.log('Decrement Click');
+  };
 
   const toggleCounterHandler = () => {
     console.log('Button Click');
@@ -12,6 +24,10 @@ const Counter = () => {
       <h1>Redux Counter</h1>
       <div className={classes.value}>COUNTER</div>
       <div className={classes.value}>{counter}</div>
+      <div className="">
+        <button onClick={incrementCounterHandler}>Increment Counter</button>
+        <button onClick={decrementCounterHandler}>Decrement Counter</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
