36d33f38507964b9319ed9fb28c029d5703a0983
Mr.Smoke <kumroshan120@gmail.com>
2023-01-30 12:54:01 +0530

    246. Reading & Dispatching From A New Slice

--- Section 18 Diving into Redux An Alternative To The Context API/Starting Up/src/components/Auth.js
+++ Section 18 Diving into Redux An Alternative To The Context API/Starting Up/src/components/Auth.js
@@ -1,17 +1,31 @@
 import classes from './Auth.module.css';
+import { useDispatch } from 'react-redux';
+import { authenticatorAction } from '../Redux/Store/index';
 
 const Auth = () => {
+  // const authenticated = useSelector(
+  //   (state) => state.authenticatorSlice.isAuthenticated
+  // );
+
+  const dispatch = useDispatch();
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+    console.log('Btn Click');
+    dispatch(authenticatorAction.login());
+  };
+
   return (
-    <main className={classes.auth}>
+    <main className={classes.auth} onSubmit={loginHandler}>
       <section>
         <form>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
           </div>
           <button>Login</button>
         </form>
--- Section 18 Diving into Redux An Alternative To The Context API/Starting Up/src/components/Header.js
+++ Section 18 Diving into Redux An Alternative To The Context API/Starting Up/src/components/Header.js
@@ -1,22 +1,36 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Header.module.css';
+import { authenticatorAction } from '../Redux/Store/index';
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const isAuth = useSelector((state) => {
+    return state.authenticatorSlice.isAuthenticated;
+  });
+
+  const logOutHandler = () => {
+    dispatch(authenticatorAction.logOut());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logOutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
